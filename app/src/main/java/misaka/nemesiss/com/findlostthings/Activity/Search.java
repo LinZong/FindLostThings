afc09e5e27f681aa5a69dcbc1cde36b0eaa62af7
Poxiao <277492866@qq.com>
2019-05-01 23:09:34 +0800

    write the search function

---
+++ app/src/main/java/misaka/nemesiss/com/findlostthings/Activity/Search.java
@@ -0,0 +1,16 @@
+package misaka.nemesiss.com.findlostthings.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import misaka.nemesiss.com.findlostthings.R;
+
+public class Search extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
