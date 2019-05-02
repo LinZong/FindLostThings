5814d02620d0503d78c4e5e773ecc0756d77dc77
Poxiao <277492866@qq.com>
2019-05-03 00:29:38 +0800

    完善搜索功能

--- app/src/main/java/misaka/nemesiss/com/findlostthings/Activity/MyPublishActivity.java
+++ app/src/main/java/misaka/nemesiss/com/findlostthings/Activity/MyPublishActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import misaka.nemesiss.com.findlostthings.R;
 import misaka.nemesiss.com.findlostthings.Model.LostThingsInfo;
 import misaka.nemesiss.com.findlostthings.Adapter.LostThingsInfoAdapter;
+import misaka.nemesiss.com.findlostthings.Tasks.MyPublishListTask;
 
 import java.util.ArrayList;
 import java.util.Base64;
@@ -24,48 +25,27 @@ public class MyPublishActivity extends FindLostThingsActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypublish);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        initLostThingsInfo();
-//        RecyclerView recyclerView = findViewById(R.id.recycler_view);
-//        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
-//        recyclerView.setLayoutManager(layoutManager);
-//        adapter = new LostThingsInfoAdapter(LostThingsInfoList);
-//        recyclerView.setAdapter(adapter);
-//
-//        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
-//        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
-//        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-//            @Override
-//            public void onRefresh() {
-//                refreshLostThingsInfo();
-//            }
-//        });
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//左侧添加一个默认的返回图标
-//        getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-    }
+        load();
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new LostThingsInfoAdapter(LostThingsInfoList,MyPublishActivity.this);
+        recyclerView.setAdapter(adapter);
 
-    public void refreshLostThingsInfo() {
-        new Thread(new Runnable() {
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
-            public void run() {
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        initLostThingsInfo();
-                        adapter.notifyDataSetChanged();
-                        swipeRefreshLayout.setRefreshing(false);
-                    }
-                });
+            public void onRefresh() {
+                load();
             }
-        }).start();
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//左侧添加一个默认的返回图标
+        getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
+
     }
 
     @Override
@@ -78,31 +58,11 @@ public class MyPublishActivity extends FindLostThingsActivity {
         return true;
     }
 
-    private void initLostThingsInfo() {
-        LostThingsInfo lostThingsInfo1 = new LostThingsInfo();
-        lostThingsInfo1.setTitle("zm");
-        lostThingsInfo1.setPhotoUrl(R.mipmap.fake_avatar);
-
-        LostThingsInfo lostThingsInfo2 = new LostThingsInfo();
-        lostThingsInfo2.setTitle("zm");
-        lostThingsInfo2.setPhotoUrl(R.drawable.add_photo);
-
-        LostThingsInfo lostThingsInfo3 = new LostThingsInfo();
-        lostThingsInfo3.setTitle("zm");
-        lostThingsInfo3.setPhotoUrl(R.drawable.add_photo);
-
-        LostThingsInfo lostThingsInfo4 = new LostThingsInfo();
-        lostThingsInfo4.setTitle("zm");
-        lostThingsInfo4.setPhotoUrl(R.drawable.add_photo);
-
-        LostThingsInfo[] lostThingsInfos = {lostThingsInfo1, lostThingsInfo2, lostThingsInfo3, lostThingsInfo4};
-
-        LostThingsInfoList.clear();
-        for (int i = 0; i < 20; i++) {
-            Random random = new Random();
-            int index = random.nextInt(lostThingsInfos.length);
-            LostThingsInfoList.add(lostThingsInfos[index]);
-        }
-
+    private void load(){
+        new MyPublishListTask(TaskRet -> {
+            if(TaskRet!=null&&TaskRet.size()!=0){
+                LostThingsInfoList.addAll(TaskRet);
+            }
+        }).execute();
     }
 }
