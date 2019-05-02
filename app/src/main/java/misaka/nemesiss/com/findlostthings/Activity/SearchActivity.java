ebfe1cf1a8726acb6442c8a8eb6a74337b2fa7a3
NemesissLin <353388639@qq.com>
2019-05-02 22:27:31 +0800

    添加一些判断请求是否成功的逻辑

--- app/src/main/java/misaka/nemesiss/com/findlostthings/Activity/SearchActivity.java
+++ app/src/main/java/misaka/nemesiss/com/findlostthings/Activity/SearchActivity.java
@@ -105,17 +105,23 @@ public class SearchActivity extends FindLostThingsActivity
     private void LoadSpinnerItems()
     {
         new GetLostThingsCategoryAsyncTask((result) -> {
-            Log.d("PickupImageActivity", "物品种类加载完成");
-            thingsCategories.clear();
-            thingsCategories.addAll(result);
-            thingsCategoryAdapter.notifyDataSetChanged();
-            Log.d("PickupImageActivity", String.valueOf(ThingCategorySpinner.getSelectedIndex()));
+            if(AppUtils.CommonResponseOK(result))
+            {
+                Log.d("PickupImageActivity", "物品种类加载完成");
+                thingsCategories.clear();
+                thingsCategories.addAll(result.getCategoryList());
+                thingsCategoryAdapter.notifyDataSetChanged();
+                Log.d("PickupImageActivity", String.valueOf(ThingCategorySpinner.getSelectedIndex()));
+            }
         }).execute();
         new GetSupportSchoolsTask((result) -> {
-            Log.d("PickupImageActivity", "支持的学校加载完成");
-            allSupportedSchool.clear();
-            allSupportedSchool.addAll(result.getSupportSchools());
-            supportedSchoolListAdapter.notifyDataSetChanged();
+            if(AppUtils.CommonResponseOK(result))
+            {
+                Log.d("PickupImageActivity", "支持的学校加载完成");
+                allSupportedSchool.clear();
+                allSupportedSchool.addAll(result.getSupportSchools());
+                supportedSchoolListAdapter.notifyDataSetChanged();
+            }
         }).execute();
     }
 
